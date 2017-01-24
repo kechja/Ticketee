be5761d6bb3144404764fd5729d7eca2531ae0f2
Kevin Jackson <kevin@kevinjacksonart.com>
2017-01-23 23:23:20 -0800

    Roles can also be assigned when admins create new users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
-
-  before_action :set_user, only: [:show, :edit, :update, :archive]
+  before_action :set_projects, only: [:new, :create, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :archive]  
 
   def index
     @users = User.excluding_archived.order(:email)
@@ -30,11 +30,19 @@ class Admin::UsersController < Admin::ApplicationController
     if params[:user][:password].blank?
       params[:user].delete(:password)
     end
-    if @user.update(user_params)
-      redirect_to(admin_users_path, :notice => "User has been updated.")
-    else
-      flash.now[:alert] = "User has not been updated."
-      render "edit"
+
+    User.transaction do
+      @user.roles.clear
+      build_roles_for(@user)
+
+      if @user.update(user_params)
+        flash[:notice] = "User has been updated."
+        redirect_to admin_users_path
+      else
+        flash.now[:alert] = "User has not been updated."
+        render "edit"
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
@@ -49,13 +57,26 @@ class Admin::UsersController < Admin::ApplicationController
   end
 
 private
-  
+ 
+  def build_roles_for(user)
+    role_data = params.fetch(:roles, [])
+    role_data.each do |project_id, role_name|
+      if role_name.present?
+        user.roles.build(project_id: project_id, role: role_name)
+      end
+    end
+  end 
+
   def set_user
     @user = User.find(params[:id])
   end
 
   def user_params
     params.require(:user).permit(:email, :password, :admin)
+  end
+
+  def set_projects
+    @projects = Project.order(:name)
   end
 
 end
