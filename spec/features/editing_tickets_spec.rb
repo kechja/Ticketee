995f22e675c89e0bec9c146ec94c4536ba2f01ef
Kevin Jackson <kevin@kevinjacksonart.com>
2017-01-24 16:07:42 -0800

    Users can now upload 3 files at a time when creating a ticket.

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -1,14 +1,16 @@
 require "rails_helper"
 
 RSpec.feature "Users can edit existing tickets" do
-
-  let(:author) { FactoryGirl.create(:user) }
-  let(:project) { FactoryGirl.create(:project)}
-  let(:ticket)  { FactoryGirl.create(:ticket, project: project, author: author) }
+  let(:author)  { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project) }
+  let(:ticket) do
+    FactoryGirl.create(:ticket, project: project, author: author)
+  end
 
   before do
     assign_role!(author, :editor, project)
     login_as(author)
+
     visit project_ticket_path(project, ticket)
     click_link "Edit Ticket"
   end
@@ -16,7 +18,9 @@ RSpec.feature "Users can edit existing tickets" do
   scenario "with valid attributes" do
     fill_in "Name", with: "Make it really shiny!"
     click_button "Update Ticket"
+
     expect(page).to have_content "Ticket has been updated."
+
     within("#ticket h2") do
       expect(page).to have_content "Make it really shiny!"
       expect(page).not_to have_content ticket.name
@@ -26,7 +30,7 @@ RSpec.feature "Users can edit existing tickets" do
   scenario "with invalid attributes" do
     fill_in "Name", with: ""
     click_button "Update Ticket"
+
     expect(page).to have_content "Ticket has not been updated."
   end
-
 end
