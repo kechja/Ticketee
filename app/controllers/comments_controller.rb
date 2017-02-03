aff267fa1677767e076553b5c4f860611cc2708a
Kevin Jackson <kevin@kevinjacksonart.com>
2017-02-02 19:43:14 -0800

    Users can add tags when adding a comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,8 +33,7 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:text, :state_id)
-    # params.require(:comment).permit(:text, :state_id, :tag_names)
+    params.require(:comment).permit(:text, :state_id, :tag_names)
   end
 
   # def sanitized_parameters
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,6 +13,10 @@ class Comment < ActiveRecord::Base
 
   before_create :set_previous_state
   after_create  :set_ticket_state
+  after_create :associate_tags_with_ticket
+  # after_create :author_watches_ticket
+
+  attr_accessor :tag_names
 
   private
 
@@ -24,5 +28,19 @@ class Comment < ActiveRecord::Base
     ticket.state = state
     ticket.save!
   end
+
+  def associate_tags_with_ticket
+    if tag_names
+      tag_names.split.each do |name|
+        ticket.tags << Tag.find_or_create_by(name: name)
+      end
+    end
+  end
+
+  # def author_watches_ticket
+  #   if author.present? && !ticket.watchers.include?(author)
+  #     ticket.watchers << author
+  #   end
+  # end
 
 end
