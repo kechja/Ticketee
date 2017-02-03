541e7f0f1f3560fe469c74c6d0b96c36eb868605
Kevin Jackson <kevin@kevinjacksonart.com>
2017-02-03 15:19:54 -0800

    Added basic email ticket notifications.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,17 @@ class CommentsController < ApplicationController
   before_action :set_ticket
 
   def create
-    @comment = @ticket.comments.build(sanitized_parameters)
-    @comment.author = current_user
+    @creator = CommentCreator.build(@ticket.comments, current_user,
+      sanitized_parameters)
+    authorize @creator.comment, :create?
 
-    # @creator = CommentCreator.build(@ticket.comments, current_user,
-
-    # authorize @creator.comment, :create?
-    authorize @comment, :create?
-
-    if @comment.save
+    if @creator.save
       flash[:notice] = "Comment has been created."
       redirect_to [@ticket.project, @ticket]
     else
       flash.now[:alert] = "Comment has not been created."
       @project = @ticket.project
-      # @comment = @creator.comment
+      @comment = @creator.comment
       render "tickets/show"
     end
   end
